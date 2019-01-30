c0a61807b96d8b7f746d2802dd6bc4956947a514
Ale <alejandrorp93@gmail.com>
2019-01-31 00:34:44 +0100

    Update index.php adding head,body and footer

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<html>
-    <head></head>
+<html lang="es">
+    <head>
+        <meta utf="8">
+        <script src="JS/test1.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="CSS/test1.css" type="text/css" >
+    </head>
     <body>
-        <h1>Hello World</h1>
+        <div id="head">
+            <h1>Hello World</h1>
+        </div>
+        <div id="body">
+
+        </div>
+        <div id="footer">
+
+        </div>
     </body>
 </html>
