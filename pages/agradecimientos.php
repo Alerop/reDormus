c16b79441d7cba0611c552a916bdd8faaa3859af
Fran994 <franciscojsumago@gmail.com>
2019-01-31 22:42:49 +0100

    Se añade pagina para agradecimientos

---
+++ pages/agradecimientos.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Pagina de agradecimiento de imagenes
+ */
+//<a href="https://icons8.com/icon/3225/user">User icon by Icons8</a>
+//<a href="https://icons8.com/icon/3307/padlock">Padlock icon by Icons8</a>
