2f4b20241eb660ade8984cfb71b15f1eb23566f1
Ale <alejandrorp93@gmail.com>
2019-02-01 06:54:26 +0100

    Se añade un par de divs necesarios para colocar los iconos usando solo un float:left en el css

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     <head>
         <meta utf="8">
         <script src="JS/test1.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="CSS/test1.css" type="text/css" >
+        <link rel="stylesheet" href="CSS/common.css" type="text/css" >
+        <link rel="stylesheet" href="CSS/index-first-page.css" type="text/css" >
     </head>
     <body>
         <div id="head">
@@ -11,14 +12,20 @@
         <div id="body">
             <div class="login">
                 <form  method="get" action="#">
-                    <img class="icons" src="https://img.icons8.com/android/24/000000/user.png">
-                    <label for="user">
-                        <input placeholder="User" name="user" type="text"/>
-                    </label>
-                    <img class="icons" src="https://img.icons8.com/android/24/000000/unlock.png">
-                    <label for="pass">
-                        <input placeholder="Password" name="pass" type="password"/>
-                    </label>
+                    <div>
+                        <img src="https://img.icons8.com/android/24/000000/user.png">
+                        <label for="user">
+                            <input placeholder="User" name="user" type="text"/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <img src="https://img.icons8.com/android/24/000000/unlock.png">
+                        <label for="pass">
+                            <input placeholder="Password" name="pass" type="password"/>
+                        </label>
+                    </div>
+
                 </form>
             </div>
         </div>
