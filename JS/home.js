b140417cabb991aeaccf72d1b0627b0eac216e99
Fran994 <franciscojsumago@gmail.com>
2019-02-13 22:03:57 +0100

    Añadido boton y funcion del registro.

---
+++ JS/home.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//    $('#registro').click(function () {
+//        window.location = 'form_login.html';
+//    });
+// });
+window.onload = function () {
+
+    document.getElementById('registro').addEventListener('click', function () {
+        window.location = 'form_login.html';
+    });
+
+};
